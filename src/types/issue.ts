72a5ccbde84d0d07dc1fa98e81f49f0c0b16074f
AVANTHIKA KS <158072314+AVA-NTHIKA14@users.noreply.github.com>
2026-02-28 17:28:07 +0530

    Refactor Issue type to include more labels and severity

--- src/types/issue.ts
+++ src/types/issue.ts
@@ -1,7 +1,11 @@
 export type Issue = {
-  label: string
   found: string
   suggestion: string
-  severity: "high" | "medium"
-  bias: "Gender Bias" | "Cultural Bias" | "Age Bias" | "Tone"
+  label:
+    | "Gender Bias"
+    | "Cultural Bias"
+    | "Age Bias"
+    | "Disability Bias"
+    | "Tone"
+  severity: "low" | "medium" | "high"
 }
